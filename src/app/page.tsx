a7e88b5af9cf099068561f0bcdf053a0e5db0fa6
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-19 02:59:15 +0000

    Updated to Geist 1.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <>
+      <h1 className="text-4xl font-semibold tracking-tight">
+        this text uses geist font, works fine on app router
+      </h1>
+      <p className="text-2xl font-semibold text-neutral-400">
+        ver. geist@1.1.0
+      </p>
+    </>
   )
 }
